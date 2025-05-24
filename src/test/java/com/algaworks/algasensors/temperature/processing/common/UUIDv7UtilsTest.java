aed35091b8157f3254ab45db9eb8e1a46b39a9f7
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2025-05-24 17:04:14 -0300

    feat: adds temperature processing data

---
+++ src/test/java/com/algaworks/algasensors/temperature/processing/common/UUIDv7UtilsTest.java
@@ -0,0 +1,25 @@
+package com.algaworks.algasensors.temperature.processing.common;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class UUIDv7UtilsTest {
+
+    @Test
+    void shouldGenerateUUIDv7(){
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
+
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        assertThat(currentDateTime).isEqualTo(uuidDateTime);
+    }
+
+
+}
